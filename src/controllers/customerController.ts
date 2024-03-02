e62bb6b4a729f53ddbc5a9502beb670ed4bccd0d
jagadeesh <jaga.deeshgnayak13@gmail.com>
2024-03-02 21:59:34 +0530

    landing page api

--- src/controllers/customerController.ts
+++ src/controllers/customerController.ts
@@ -54,6 +54,40 @@ export const createNewCustomer = async (
   }
 };
 
+export const getDeliveryOption = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  try {
+    // Hardcoded delivery option data
+    const deliveryOption = {
+      success: true,
+      message: "Select option for instat deliver or next day delivery",
+      data: {
+        promotions: [
+          {
+            image_url: "https://scontent.fblr1-6.fna.fbcdn.net/v/t39.30808-6/303510680_487254480079549_4133931268015615273_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=efb6e6&_nc_ohc=B6rA_iOxvPEAX9XUOnm&_nc_ht=scontent.fblr1-6.fna&oh=00_AfDc16_DioPQldOFijRMCVoMV-NIS-yFYSkEjq-LhtXDXw&oe=65E2FF95",
+            id: 1,
+            type: "Instant",
+          },
+          {
+            image_url: "https://scontent.fblr1-6.fna.fbcdn.net/v/t39.30808-6/303510680_487254480079549_4133931268015615273_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=efb6e6&_nc_ohc=B6rA_iOxvPEAX9XUOnm&_nc_ht=scontent.fblr1-6.fna&oh=00_AfDc16_DioPQldOFijRMCVoMV-NIS-yFYSkEjq-LhtXDXw&oe=65E2FF95",
+            id: 2,
+            type: "NextDayDelivery",
+          },
+        ],
+        cart_count: 0,
+        notification_count: 0,
+      },
+    };
+    // Respond with the delivery option JSON
+    res.json(deliveryOption);
+  } catch (error) {
+    console.error("Error:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
 /*
 firebaseId: string,
 first_name: string,
--- src/routes/customerRoutes.ts
+++ src/routes/customerRoutes.ts
@@ -3,12 +3,14 @@ import {
   checkIfCustomerExists,
   fetchCustomerData,
   createNewCustomer,
+  getDeliveryOption,
 } from "../controllers/customerController";
 import isAuth from "../middleware/isAuth";
 const customerRoutes = Router();
 
 customerRoutes.get("/customerExists/:firebaseId", checkIfCustomerExists);
 customerRoutes.get("/customerInfo/:firebaseId", fetchCustomerData);
+customerRoutes.get("/deliveryoption/:firebaseId", getDeliveryOption);
 
 customerRoutes.post("/newCustomer", createNewCustomer);
 
